cfaf8c787b0cad1aeca18e9120c03c4e3f279431
Gabriel-Bonato <gabrielbbonato7@hotmail.com>
2024-06-02 16:47:00 -0300

    tela de login "pronta" a parte de html e css

--- View/login.php
+++ View/login.php
@@ -1,20 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./styleLogin.css">
 </head>
+
 <body>
-    <form action="autenticar.php" method="post">
-        <input type="text" name="loginUsu">
-        <input type="text" name="senhaUSu">
-        <input type="submit" value="Entrar">
-    </form>
+    <main>
+        <section>
+            <h1>Fazer Login</h1>
+            <form action="autenticar.php" method="post">
+
+                <input type="text" name="loginUsu" placeholder="Login/Usuario">
+                <br><br>
+
+                <input type="password" name="senhaUSu" placeholder="Senha">
+                <br><br>
+
+                <input id="button" type="submit" value="Entrar">
+            </form>
+        </section>
+    </main>
     <?php
     if (isset($_GET['erro'])) {
         echo "<p style='color:red;'>Nome de usuário ou senha incorretos.</p>";
     }
     ?>
 </body>
+
 </html>
